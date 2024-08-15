14ed139a5326a09716a21f2a81a6df0a72302d3b
Shreya Sarkar <srkr4436@gmail.com>
2024-08-16 00:14:36 +0530

    project 4 ongoing

--- react-practice-4/front-end/src/App.jsx
+++ react-practice-4/front-end/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="main_container">
+      <header>
+        <h1 className="page_name">
+          F<span>re</span>shly M<span>a</span>de
+        </h1>
+
+        <div className="search_bar">
+          <input type="text" placeholder="Search Food...." />
+        </div>
+      </header>
+
+      <ul className="filter_wrap">
+        <li>All</li>
+        <li>Breakfast</li>
+        <li>Lunch</li>
+        <li>Dinner</li>
+        <li>Dessert</li>
+      </ul>
+
+      <div className="food_wrap">
+        <div className="food_grid">
+          <div className="food_cart">bbfsjskhsekfhkshfksdfsdk</div>
+          <div className="food_cart">bbfsjskhsekfhkshfksdfsdk</div>
+          <div className="food_cart">bbfsjskhsekfhkshfksdfsdk</div>
+          <div className="food_cart">bbfsjskhsekfhkshfksdfsdk</div>
+          <div className="food_cart">bbfsjskhsekfhkshfksdfsdk</div>
+          <div className="food_cart">bbfsjskhsekfhkshfksdfsdk</div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
