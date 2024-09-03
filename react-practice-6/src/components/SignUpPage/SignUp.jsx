84b2731ff77249cb8d191d33d0b1ba530fbba5f8
Shreya Sarkar <srkr4436@gmail.com>
2024-09-04 01:52:08 +0530

    signUp page WIP

--- react-practice-6/src/components/SignUpPage/SignUp.jsx
+++ react-practice-6/src/components/SignUpPage/SignUp.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import styles from "./SignUp.module.scss";
 
 function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.form_container}>
+        <div className={styles.form_heading}>
+          <h1>Welcome to Crypto App</h1>
+          <p className="grey">Create a free account by filling data below.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SignUp;
