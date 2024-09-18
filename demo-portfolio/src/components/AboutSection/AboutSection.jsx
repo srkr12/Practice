c46d529e7886f66e255df1c5df3176939787190e
Shreya Sarkar <srkr4436@gmail.com>
2024-09-18 22:55:54 +0530

    about section wip

--- demo-portfolio/src/components/AboutSection/AboutSection.jsx
+++ demo-portfolio/src/components/AboutSection/AboutSection.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import styles from "./AboutSection.module.scss";
 
 function AboutSection() {
-  return <div>AboutSection</div>;
+  return (
+    <div className={styles.about_sec_container}>
+      <div className={styles.heading}>
+        <span className="route_tab body2_mid">About me</span>
+      </div>
+
+      <div className={styles.content_wrap}>
+        <div className={styles.img_wrap}>
+          <img src="./images/programmer.png" alt="programmer image" />
+        </div>
+
+        <div className={styles.text_wrap}>
+          <h3 className="mid_heading red">Want to know more about me?</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AboutSection;
