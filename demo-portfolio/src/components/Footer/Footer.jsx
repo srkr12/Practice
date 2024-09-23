8b71b4dfd67aac03d5a9b0b64cf91926ef63b63d
Shreya Sarkar <srkr4436@gmail.com>
2024-09-23 14:18:26 +0530

    footer done

--- demo-portfolio/src/components/Footer/Footer.jsx
+++ demo-portfolio/src/components/Footer/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import styles from "./Footer.module.scss";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.footer_container}>
+      <span className="grey">© 2024 |</span>{" "}
+      <span className="red">Demo project</span>
+    </footer>
+  );
 }
 
 export default Footer;
